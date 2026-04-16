50946af8b2d7a69313261570351f34be621977ef
TousifTamboli <tousiftamboli3@gmail.com>
2026-04-17 01:21:29 +0530

    docker changes

--- flask-fastapi-project/fastapi_app.py
+++ flask-fastapi-project/fastapi_app.py
@@ -1,8 +1,11 @@
-# fastapi_app.py
 from fastapi import FastAPI
 
 api = FastAPI()
 
+@api.get("/")
+def root():
+    return {"message": "Hi from root 🚀"}
+
 @api.get("/api")
 def get_message():
-    return {"message": "Hi from FastAPI 🚀"}
+    return {"message": "Hi from API"}
